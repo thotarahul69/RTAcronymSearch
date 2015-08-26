2ab562a10a44d0afc436611427c53295609ad30c
Rahul Thota <thotarahul69@gmail.com>
2015-08-26 17:46:06 -0600

    Initial Commit

---
+++ RTAcronymSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RTAcronymSearch
+//
+//  Created by Nanda Gundapaneni on 8/26/15.
+//  Copyright (c) 2015 Rahul Thota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RTAcronymSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RTAcronymSearch
+//
+//  Created by Nanda Gundapaneni on 8/26/15.
+//  Copyright (c) 2015 Rahul Thota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
